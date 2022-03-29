be439e07ef9e43691275f64f186062af51ca63c0
adilahmed31@gmail.com <adilahmed31@gmail.com>
2022-03-28 23:52:31 -0500

    Added framework for writing python workload tests

--- client.cc
+++ client.cc
@@ -12,17 +12,19 @@ static struct options {
     std::string primary = ip_server_wifs_1;
 } options;
 
-extern "C"{
-int init(std::string server_ip=ip_server_wifs_1){
-    options.wifsclient = new WifsClient(grpc::CreateChannel(server_ip, grpc::InsecureChannelCredentials()));
-}
+extern "C" {
+
+int init(const char* server_ip = ip_server_wifs_1.c_str()){
+	std::string ip_address(server_ip);
+    options.wifsclient = new WifsClient(grpc::CreateChannel(ip_address, grpc::InsecureChannelCredentials()));
 }
 
 int do_read(int address, char* buf) {
     int rc = options.wifsclient->wifs_READ(address, buf);
+
     if(rc < 0){
-	init(ip_server_wifs_2);
-	do_read(address, buf);
+		init(ip_server_wifs_2.c_str());
+		do_read(address, buf);
     }
     return rc;    
 }
@@ -30,10 +32,12 @@ int do_read(int address, char* buf) {
 int do_write(int address, char* buf) {
     int rc = options.wifsclient->wifs_WRITE(address, buf);
     if(rc<0){
-        init(ip_server_wifs_2);
+		init(ip_server_wifs_2.c_str());
         do_write(address, buf);
     }
     return rc;
+}
+
 }
 	
 
