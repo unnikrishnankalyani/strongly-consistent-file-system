4b16364b578eadce998a061aeef46e14e73d3e74
Basava <basava726.k@gmail.com>
2022-03-31 17:35:42 -0500

    tested, code works

--- client_manual.cc
+++ client_manual.cc
@@ -11,18 +11,24 @@ void tester() {
     char buf[BLOCK_SIZE + 1];
     for (int i = 0; i < BLOCK_SIZE; i++) buf[i] = 'c';
     int rc = do_write(0, buf);
-    if(rc == -1) std::cout << "WRITE FAIL\n";
-    
+    if (rc == -1) std::cout << "WRITE FAIL\n";
+
     buf[0] = '\0';
     rc = do_read(0, buf);
-    if(rc == -1) std::cout << "READ FAIL\n";
+    if (rc == -1) std::cout << "READ FAIL\n";
 
     buf[BLOCK_SIZE] = '\0';
     printf("read first char - %c\n", buf[0]);
+
+    do_read(0, buf);
+    if (rc == -1) std::cout << "READ FAIL\n";
+    do_read(0, buf);
+    if (rc == -1) std::cout << "READ FAIL\n";
+    do_read(0, buf);
+    if (rc == -1) std::cout << "READ FAIL\n";
 }
 
 int main(int argc, char* argv[]) {
-    //options.wifsclient = new WifsClient(grpc::CreateChannel("localhost:50055", grpc::InsecureChannelCredentials()));
     tester();
     return 0;
 }
